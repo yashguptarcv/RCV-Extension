a01a4fc773d62e120a1f8f47aa568d8131f619c9
Yash Gupta <yash.gupta@rcvtechnologies.com>
2025-06-25 18:27:12 +0530

    Added command to take screenshot

--- rcv-snapPro/background.js
+++ rcv-snapPro/background.js
@@ -1,4 +1,17 @@
-chrome.action.onClicked.addListener(() => {
+// Handle extension icon click
+chrome.action.onClicked.addListener((tab) => {
+  captureScreenshotAndOpenEditor();
+});
+
+// Handle keyboard shortcut (Alt+S)
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'take-screenshot') {
+    captureScreenshotAndOpenEditor();
+  }
+});
+
+// Shared screenshot function
+function captureScreenshotAndOpenEditor() {
   chrome.tabs.captureVisibleTab(null, { format: "png" }, (dataUrl) => {
     chrome.storage.local.set({ screenshot: dataUrl }, () => {
       chrome.tabs.create({
@@ -6,4 +19,4 @@ chrome.action.onClicked.addListener(() => {
       });
     });
   });
-});
+}
